9b5b253971348dfb0ffd324f45fd02c2698cd484
Alfrendo Silalahi <alfrendos72@gmail.com>
2026-02-28 13:29:26 +0700

    add jwt middleware

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { Hono } from "@hono/hono";
 import * as usersService from "./modules/users/users.service.ts";
 import globalErrorHandler from "./errors/handlers/global-error.handler.ts";
+import { protect } from "./middlewares/auth.middlware.ts";
 
 const app = new Hono();
 
 app.get("/", (c) => c.text("Welcome to Hono!"));
-app.get("/api/users", usersService.getUsers);
-app.post("/api/users", usersService.createUser);
-app.get("/api/users/:id", usersService.getUser);
+app.get("/api/users", protect, usersService.getUsers);
+app.post("/api/users", protect, usersService.createUser);
+app.get("/api/users/:id", protect, usersService.getUser);
 
 app.onError(globalErrorHandler);
 
--- src/middlewares/auth.middlware.ts
+++ src/middlewares/auth.middlware.ts
@@ -1,6 +1,17 @@
 import { Context, Next } from "@hono/hono";
+import { verify } from "@hono/hono/jwt";
 
-export const protect = (_c: Context, next: Next) => {
-  console.log("auth middleware");
-  next();
+const secret = Deno.env.get("JWT_SECRET") || "";
+
+export const protect = async (c: Context, next: Next) => {
+  const authorization = c.req.header("Authorization");
+  if (authorization === undefined || !authorization.startsWith("Bearer "))
+    throw Error("Auhentication failed.");
+
+  const token = authorization.substring(7);
+
+  const payload = await verify(token, secret, "HS256");
+  console.log(payload);
+
+  await next();
 };
